0c368cef165a27bc0bd9878b5155ccc24c476468
Thomas <tm009117@ohio.edu>
2019-01-22 11:10:36 -0500

    update git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/********************************************
+*
+*	File: git-lab-program.cc
+*     Author: Thomas Magalski
+*       Date: 01/22/2019
+*Description: Learning to use git hub.
+*
+*********************************************/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}
